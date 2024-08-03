3ff363c7d67800111d444600f3ebf5b41406b971
Mirembe Peace Mercy <peace2de@gmail.com>
2024-08-03 17:52:10 +0300

    trials

--- trials.c
+++ trials.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int ReadVal();
 float Convert(int F);
-float printResult(int f,int c);
+float printResult(int f,float c);
 
 int main(){
     int EndValue = ReadVal();
@@ -17,7 +17,7 @@ return 0;
 
 int ReadVal(){
     int EndVal;
-    printf("Enter the last value you want to convert");
+    printf("Enter the last value you want to convert:");
     scanf("%d",&EndVal);
     return EndVal;
 }
@@ -27,6 +27,6 @@ int C = (F-32)/1.8;
 return C;
 }
 
-float printResult(int f, int c){
-    printf("%10.2f %10.2f\n",f,c);
+float printResult(int f, float c){
+    printf("%10.2d %10.2f\n",f,c);
 }
