cd8cb2d7ea7bd4fe662595f13b22b9359a56f499
Mirembe Peace Mercy <peace2de@gmail.com>
2024-08-03 18:21:05 +0300

    trials

--- loop1.c
+++ loop1.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main(){
+    for(int i=1; i<=3; i++){
     for(int x=1; x<=12; x++){
-        printf("\xB2");
+        printf("\xB2\n");
     }
-    printf("\n");
+    }
         return 0;
 }
